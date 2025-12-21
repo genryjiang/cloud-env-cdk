c1277e3acf414dfffb9a0b6e7f6f0a7c0a411bac
Henry Jiang <z5416365@ad.unsw.edu.au>
2025-12-21 18:39:05 +1100

    added prelim dockerfile

--- lib/srp-130_aws_infra-stack.ts
+++ lib/srp-130_aws_infra-stack.ts
@@ -1,13 +1,17 @@
-import { Duration, Stack, StackProps } from 'aws-cdk-lib';
-import * as sns from 'aws-cdk-lib/aws-sns';
-import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
-import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { CfnOutput, Duration, RemovalPolicy, Stack, StackProps } from 'aws-cdk-lib';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as codebuild from 'aws-cdk-lib/aws-codebuild';
+// Cloudwatch pipe
+import * as ecr from 'aws-cdk-lib/aws-ecr';
+import * as iam from 'aws-cdk-lib/aws-iam';
 import { Construct } from 'constructs';
 
 export class Srp130AwsInfraStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
+    // SAMPLE - DELETE LATER
+    /*
     const queue = new sqs.Queue(this, 'Srp130AwsInfraQueue', {
       visibilityTimeout: Duration.seconds(300)
     });
@@ -15,5 +19,34 @@ export class Srp130AwsInfraStack extends Stack {
     const topic = new sns.Topic(this, 'Srp130AwsInfraTopic');
 
     topic.addSubscription(new subs.SqsSubscription(queue));
+    */
+
+    const sdp_s3 = new s3.Bucket(this,'embd-high-level-infra', {
+      bucketName: `sr8-embd-dev-env-${this.region}`,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      encryption: s3.BucketEncryption.S3_MANAGED,
+      versioned: true,
+      removalPolicy: RemovalPolicy.RETAIN,
+      autoDeleteObjects: false
+    });
+
+    // easy reference bucket name (from tutorial)
+    new CfnOutput(this, "SRP8-130_Bucket", {
+      value: sdp_s3.bucketName,
+      description: 'S3 Bucket to store cloud infra for SRP8-130'
+    })
+
+    // No docker image scanning needed - this isn't an application that isn't exposed to the public (just a dev environment)
+    const embd_ecr = new ecr.Repository(this, "embd_dev_ecr", {
+        imageTagMutability: ecr.TagMutability.IMMUTABLE_WITH_EXCLUSION,
+    });
+    const user = new iam.User(this, 'User');
+    ecr.AuthorizationToken.grantRead(user);
+
+    /*const dev_env_codebuild = new codebuild.Project(this, 'SR8-EMBD-Dev', {
+      buildSpec: // ADD BUILDSPEC to build docker container
+
+    });
+    */
   }
 }
